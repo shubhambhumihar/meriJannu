0686ed3987d6432d654f215e37252722da60d332
Shubham <sk4957199@gmail.com>
2022-09-28 19:33:41 +0530

    janu chlo dom padhte hai

---
+++ lecture1/index.js
@@ -0,0 +1,26 @@
+const person = {
+  name: "Shubham",
+  branch: "CSE",
+  address: "Siwan",
+  isGf: " Yes i have a new gf",
+  nameOfgF: " Kolakta me hai name nahi batunga",
+  haveS: "Yes i have done one times ",
+};
+
+console.log(
+  `my name is ${person.name} and my branch is ${person.branch}. i live in ${person.address}. ${person.isGf} ${person.nameOfgF} .${person.haveS} `
+);
+
+// console.log(
+//   "my name is " +
+//     person.name +
+//     " and my branch is " +
+//     person.branch +
+//     " and i live in " +
+//     person.address +
+//     person.isGf +
+//     " name of my gf is " +
+//     person.nameOfgF +
+//     " do u have done anytime s.." +
+//     person.haveS
+// );
